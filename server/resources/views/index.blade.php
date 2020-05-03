3be319bee087509e45d1d3d851d8d5be62cbba4f
tylergreulich <tgreul6@gmail.com>
2020-05-02 20:55:48 -0700

    optimized backend code

--- server/resources/views/index.blade.php
+++ server/resources/views/index.blade.php
@@ -7,7 +7,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <title>Larticles App</title>
 
--- server/resources/views/pages/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('../layouts.app')
-
-@section('content')
-  <div class="welcome-jumbo">
-    <h1>Contact</h1>
-  </div>
-@endsection
